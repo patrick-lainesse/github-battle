1afcfb8b8dee0bf911cb5fa9b5f2037c1211ba88
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-08-17 10:14:58 -0400

    Initial commit. Manually setup the first with npm and webpack to display Hello World.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+// Plugin to recreate index.html from the app directory to the dist directory and autonomously referencing script
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {test: /\.(js)$/, use: 'babel-loader'},
+            {test: /\.(css)$/, use: ['style-loader', 'css-loader']},
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'app/index.html'
+        })
+    ]
+};
