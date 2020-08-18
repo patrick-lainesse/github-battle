2be83eff89cb28cb15921d4a19ec4c1bfdee285e
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-08-18 11:57:56 -0400

    feat: Styling repositories list display

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {fetchPopularRepos} from "../utils/api";
+import {FaUser, FaStar, FaCodeBranch, FaExclamationTriangle} from 'react-icons/fa'
 
 function LanguagesNav({selected, onUpdateLanguage}) {
 
@@ -23,9 +24,63 @@ function LanguagesNav({selected, onUpdateLanguage}) {
     )
 }
 
+/*defining what type should the props be*/
 LanguagesNav.propTypes = {
     selected: PropTypes.string.isRequired,
     onUpdateLanguage: PropTypes.func.isRequired
+}
+
+// Display the fetched repositories in a grid
+function ReposGrid({repos}) {
+    return (
+        // Loop over each repo of the array and display in a card
+        <ul className='grid space-around'>
+            {repos.map((repo, index) => {
+                const {name, owner, html_url, stargazers_count, forks, open_issues} = repo
+                const {login, avatar_url} = owner
+
+                return (
+                    <li key={html_url} className='repo bg-light'>
+                        <h4 className='header-lg center-text'>
+                            #{index + 1}
+                        </h4>
+                        <img
+                        className='avatar'
+                        src={avatar_url}
+                        alt={'Avatar for ${login}'}
+                        />
+                        <h2 className='center-text'>
+                            <a className='link' href={html_url}>{login}</a>
+                        </h2>
+                        <ul className='card-list'>
+                            <li>
+                                <FaUser color='rgb(255, 191, 116)' size={22} />
+                                <a href={'https://github.com/${login}'}>
+                                    {login}
+                                </a>
+                            </li>
+                            <li>
+                                <FaStar color='rgb(255, 215, 0)' size={22}/>
+                                {stargazers_count.toLocaleString()} stars
+                            </li>
+                            <li>
+                                <FaCodeBranch color='rgb(129, 195, 245)' size={22}/>
+                                {forks.toLocaleString()} forks
+                            </li>
+                            <li>
+                                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22}/>
+                                {open_issues.toLocaleString()} open
+                            </li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+ReposGrid.propTypes = {
+    repos: PropTypes.array.isRequired
 }
 
 export default class Popular extends React.Component {
@@ -79,7 +134,7 @@ export default class Popular extends React.Component {
         const {selectedLanguage, repos, error} = this.state
 
         // Loading only if we do not already have the data for the selected language in our state object
-        return  !repos[selectedLanguage] && error === null
+        return !repos[selectedLanguage] && error === null
     }
 
     render() {
@@ -97,8 +152,8 @@ export default class Popular extends React.Component {
 
                 {error && <p>{error}</p>}
 
-                {/*Display fetched repos without styling them, only if selected language is a thing in repos*/}
-                {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+                {/*Display fetched repos in a grid*/}
+                {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]}/>}
             </React.Fragment>
         )
     }
