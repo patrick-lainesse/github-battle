3e6f06c16f1a2b35efdc2126658e20df8d3eebe8
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-08-18 14:41:53 -0400

    feat: Added controlled input component
    The input is handled by React, in the sense that everything that is typed by the user updates the state of the PlayerInput component.

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {FaUserFriends, FaFighterJet, FaTrophy} from 'react-icons/fa';
+import PropTypes from 'prop-types'
 
 function Instructions() {
     return (
@@ -10,19 +11,84 @@ function Instructions() {
             <ol className='container-sm grid center-text battle-instructions'>
                 <li>
                     <h3 className='header-sm'>Enter two GitHub users</h3>
-                    <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
+                    <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140}/>
                 </li>
                 <li>
                     <h3 className='header-sm'>Battle</h3>
-                    <FaFighterJet className='bg-light' color='#727272' size={140} />
+                    <FaFighterJet className='bg-light' color='#727272' size={140}/>
                 </li>
                 <li>
                     <h3 className='header-sm'>See the winners</h3>
-                    <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
+                    <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140}/>
                 </li>
             </ol>
         </div>
     )
+}
+
+/**
+ * Component to get the username from the input field
+ */
+class PlayerInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleSubmit(event) {
+        // Prevents the normal browser event to take place
+        event.preventDefault()
+
+        this.props.onSubmit(this.state.username)
+    }
+
+    // Gets the value that the user typed in and updates the state of the component
+    handleChange(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <form className='column player' onSubmit={this.handleSubmit}>
+                <label htmlFor='username' className='player-label'>
+                    {this.props.label}
+                </label>
+                <div className='row player-inputs'>
+                    <input
+                        type='text'
+                        id='username'
+                        className='input-light'
+                        placeholder='github username'
+                        autoComplete='off'
+                        /*Since we are building a controlled component, the value resides in the state of the component
+                          and is updated through handleChange*/
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <button
+                        className='btn dark-btn'
+                        type='submit'
+                        disabled={!this.state.username}
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        )
+    }
+}
+
+PlayerInput.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired
 }
 
 export default class Battle extends React.Component {
@@ -30,6 +96,9 @@ export default class Battle extends React.Component {
         return (
             <React.Fragment>
                 <Instructions/>
+
+                {/*for test purposes*/}
+                <PlayerInput onSubmit={(value) => console.log('value!', value)} label='label'/>
             </React.Fragment>
         )
     }
