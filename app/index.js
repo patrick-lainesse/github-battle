a73abac065aa64e52601642f6f67ba5fc0ec77f5
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-08-21 09:26:07 -0400

    feat: 404 page
    Implemented a switch in index.js so that a request that doesn't match any path is rendering a 404 error.

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ import Battle from './components/Battle'
 import Results from "./components/Results";
 import {ThemeProvider} from "./contexts/theme";
 import Nav from './components/Nav'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 class App extends React.Component {
     constructor(props) {
@@ -31,9 +31,14 @@ class App extends React.Component {
                         <div className='container'>
                             <Nav/>
 
-                            <Route exact path='/' component={Popular}/>
-                            <Route exact path='/battle' component={Battle}/>
-                            <Route path='/battle/results' component={Results}/>
+                            {/*Switch is used so that only the first route that matches is rendered*/}
+                            <Switch>
+                                <Route exact path='/' component={Popular}/>
+                                <Route exact path='/battle' component={Battle}/>
+                                <Route path='/battle/results' component={Results}/>
+                                {/*Route without path that will always match if none of the above matched, rendered inline JSX*/}
+                                <Route render={() => <h1>404</h1>}/>
+                            </Switch>
                         </div>
                     </div>
                 </ThemeProvider>
