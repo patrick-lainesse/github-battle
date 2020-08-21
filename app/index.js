7d172ac0df2c32d56cb3b88b9277585376de9cf9
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-08-21 11:42:26 -0400

    feat: Dynamic import of modules
    Installed syntax-dynamic-import plugin from Babel so that the imports downloads are delayed until they are necessary, in order to increase application performance.

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Popular from './components/Popular';
-import Battle from './components/Battle'
-import Results from "./components/Results";
-import {ThemeProvider} from "./contexts/theme";
+import {ThemeProvider} from './contexts/theme'
 import Nav from './components/Nav'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Loading from './components/Loading'
+
+/* Makes sure we don't import our components until they are needed for rendering.
+   React.lazy renders a dynamic import as a regular component. */
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Results'))
 
 class App extends React.Component {
     state = {
@@ -27,14 +31,17 @@ class App extends React.Component {
                         <div className='container'>
                             <Nav/>
 
-                            {/*Switch is used so that only the first route that matches is rendered*/}
-                            <Switch>
-                                <Route exact path='/' component={Popular}/>
-                                <Route exact path='/battle' component={Battle}/>
-                                <Route path='/battle/results' component={Results}/>
-                                {/*Route without path that will always match if none of the above matched, rendered inline JSX*/}
-                                <Route render={() => <h1>404</h1>}/>
-                            </Switch>
+                            {/*Suspense fallback will show this if one of our component takes too long to load*/}
+                            <React.Suspense fallback={<Loading/>}>
+                                {/*Switch is used so that only the first route that matches is rendered*/}
+                                <Switch>
+                                    <Route exact path='/' component={Popular}/>
+                                    <Route exact path='/battle' component={Battle}/>
+                                    <Route path='/battle/results' component={Results}/>
+                                    {/*Route without path that will always match if none of the above matched, rendered inline JSX*/}
+                                    <Route render={() => <h1>404</h1>}/>
+                                </Switch>
+                            </React.Suspense>
                         </div>
                     </div>
                 </ThemeProvider>
